e3ecf925192d71a2b21fba11dfaf9653f1547a8a
Michael Lester <lesterfx@gmail.com>
2020-10-18 21:04:19 -0700

    renamed halloween to fairy

---
+++ holidaypixels/animations/fairy.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+from __future__ import division
+import random
+import time
+
+from ..utilities import physics
+from ..utilities.home import Home, Color, Pixel
+
+class Halloween (Home):
+    def main(self):
+        self.system = physics.System(self)
+        self.system.effects.append(physics.Random_Speed(-3000, 3000))
+        self.system.effects.append(physics.Random_Intensity((-.01, .007), (0, 1)))
+
+        pos = random.randrange(len(self))
+        speed = random.uniform(-30, 30)
+        color = Color(random.random())
+        self.system.particles.add(physics.Particle(speed=speed, position=pos, color=color, strip=self))
+
+        while self.system.update_and_draw():
+            pass
+    
+    def __str__(self):
+        return 'fairy'
