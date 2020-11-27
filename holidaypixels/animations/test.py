508b96f9566868f9de54e5cdd1755d9fbc6aa092
Michael Lester <lesterfx@gmail.com>
2020-11-26 17:50:40 -0800

    test shows corners

--- holidaypixels/animations/test.py
+++ holidaypixels/animations/test.py
@@ -19,14 +19,13 @@ class Animation(object):
     def main(self, end_by):
         max_ = self.globals.ranges[-1][1]
         for x in range(0, max_, 5):
-            self.home[x] = Color(0, 0, 1)
+            self.home[x] = Color(0, 0, 0.5)
         for x in range(0, max_, 10):
-            self.home[x] = Color(0, 1, 0)
+            self.home[x] = Color(0, 0.5, 0)
         for x in range(0, max_, 50):
-            self.home[x] = Color(1, 0, 0)
-        for range_ in self.globals.ranges:
-            self.home[range_[0]] = Color(1, 1, 1)
-            self.home[range_[1]-1] = Color(1, 1, 1)
+            self.home[x] = Color(0.5, 0, 0)
+        for corner in self.globals.corners:
+            self.home[corner] = Color(1, 1, 1)
         while datetime.datetime.now() < end_by:
             self.home.show()
             time.sleep(1)
