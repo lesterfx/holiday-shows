b09528aa77be8cc469cb4db78cc951b26683207a
Michael Lester <lesterfx@gmail.com>
2020-11-26 23:35:30 -0800

    test just shows black and corners

--- holidaypixels/animations/test.py
+++ holidaypixels/animations/test.py
@@ -17,15 +17,15 @@ class Animation(object):
         return 'Test'
 
     def main(self, end_by):
-        max_ = self.globals.ranges[-1][1]
-        digits = len(bin(max_)) - 1
-        for x in range(0, max_, digits):
-            num = bin(x)[2:]
-            for i, val in enumerate(num):
-                if val == '1':
-                    self.home[x+i] = Color(1, 0, 0)
-                elif val == '0':
-                    self.home[x+i] = Color(0, 1, 0)
+        max_ = self.globals.max
+        # digits = len(bin(max_)) - 1
+        # for x in range(0, max_, digits):
+        #     num = bin(x)[2:]
+        #     for i, val in enumerate(num):
+        #         if val == '1':
+        #             self.home[x+i] = Color(1, 0, 0)
+        #         elif val == '0':
+        #             self.home[x+i] = Color(0, 1, 0)
 
         for corner in self.globals.corners:
             self.home[corner] = Color(0, 0, 1)
