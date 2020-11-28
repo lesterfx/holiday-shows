4f204fecee09f338becba747ce79f3e6009925fa
Michael Lester <lesterfx@gmail.com>
2020-11-28 14:08:49 -0800

    confirmed working, back to run on :00

--- holidaypixels/animations/image.py
+++ holidaypixels/animations/image.py
@@ -38,7 +38,7 @@ class Animation(object):
         self.activate_relays()
         now = datetime.datetime.now()
         while now <= end_by:
-            while now.minute != 7:
+            while now.minute != 0:
                 print('minute is', now.minute)
                 time.sleep(10)
                 now = datetime.datetime.now()
