fc6bba4865635a12f2fdbbbc35ad8831d35de83b
Michael Lester <lesterfx@gmail.com>
2020-11-29 16:30:52 -0800

    simple xmas between animations

--- holidaypixels/animations/simple_xmas.py
+++ holidaypixels/animations/simple_xmas.py
@@ -1,3 +1,4 @@
+import datetime
 import time
 
 from ..utilities.home import Color
@@ -12,12 +13,17 @@ class Animation(object):
         return 'Simple Xmas'
 
     def main(self, end_by=None):
-        offset = time.time() % 10
-        for x in range(self.globals.max):
-            pos = (x + offset) % 10
-            self.home.clear()
-            if pos == 0:
-                self.home[x] = Color(1, 0, 0)
-            elif pos == 5:
-                self.home[x] = Color(1, 1, 1)
+        while True:
+            offset = time.time() % 10
+            for x in range(self.globals.max):
+                pos = (x + offset) % 10
+                self.home.clear()
+                if pos == 0:
+                    self.home[x] = Color(1, 0, 0)
+                elif pos == 5:
+                    self.home[x] = Color(1, 1, 1)
             self.home.show()
+            if end_by is None:
+                return
+            elif datetime.datetime.now() >= end_by:
+                break
