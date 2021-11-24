50da95c8f50a1a75f088b689c0bc10049ed373bd
Michael Lester <lesterfx@gmail.com>
2021-11-24 14:50:15 -0800

    transition to remote relays

--- holidaypixels/utilities/arduino_test_b.py
+++ holidaypixels/utilities/arduino_test_b.py
@@ -18,12 +18,16 @@ class ArduinoTest:
             now = time.time()
             if now - start > 1:
                 fps = counted / (now - start)
-                print(f'{fps:.5f} fps')
+                print(f'            {fps:.5f} fps')
                 start = now
                 counted = 0
             for sock in self.socks:
                 # i = 0
-                msg = f'{(i % 16):02d}:0,{((i+1) % 16):02d}:1\n'
+                if i % 2 == 0:
+                    msg = 'all0\n'
+                else:
+                    msg = 'all1\n'
+                # msg = f'{(i % 16):02d}:0,{((i+1) % 16):02d}:1\n'
                 while not self.ready_to_receive(sock):
                     print('waiting')
                     time.sleep(0.01)
@@ -35,6 +39,7 @@ class ArduinoTest:
             data = sock.recv(1024)
         except BlockingIOError:
             return False
+        print('<', data.decode(), end='')
         return True
 
     def wait_for_ready(self, sock):
@@ -43,6 +48,7 @@ class ArduinoTest:
 
     def setup(self):
         ips = ['192.168.3.241']#, '192.168.3.240', '192.168.3.242']
+        ips = ['192.168.1.17']
         self.socks = []
         for ip in ips:
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
