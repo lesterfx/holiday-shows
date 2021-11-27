efa5cb44d4f373f7e0a4b9a277bc5c8a8011f189
Michael Lester <lesterfx@gmail.com>
2021-11-27 11:47:47 -0800

    sarajevo and sugarplum!

--- holidaypixels.py
+++ holidaypixels.py
@@ -72,6 +72,8 @@ class Holiday_Pixels(object):
                 self.demo(self.args.demo)
             else:
                 self.main()
+        except KeyboardInterrupt:
+            pass
         finally:
             self.strip.on = False
 
