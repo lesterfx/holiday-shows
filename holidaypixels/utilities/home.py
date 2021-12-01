235f2e2c983a3e51259b7827f31fd3c3a22e97dd
Michael Lester <lesterfx@gmail.com>
2021-11-30 17:18:17 -0800

    sarajevo and sugarplum!

--- holidaypixels/utilities/home.py
+++ holidaypixels/utilities/home.py
@@ -344,6 +344,7 @@ class Strip_Remote_Client():
         if self.ip:
             width = len(image_data[0])
             height = len(image_data)
+            print(f'{self.name}: loading image {index} ({width}x{height})')
             packed = struct.pack('iii', index, width, height)
             for row in image_data:
                 for pixel in row:
