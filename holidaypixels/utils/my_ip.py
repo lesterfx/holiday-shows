e1bbb1bea82be3fca989800cfd9ecc9c8582923c
Michael Lester <lesterfx@gmail.com>
2021-12-12 21:37:53 -0800

    organized code

---
+++ holidaypixels/utils/my_ip.py
@@ -0,0 +1,15 @@
+import socket
+
+def my_ip():
+    # https://stackoverflow.com/a/28950776/3130539
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        ip = s.getsockname()[0]
+    except Exception:
+        ip = '127.0.0.1'
+    finally:
+        s.close()
+    return ip
+MY_IP = my_ip()
